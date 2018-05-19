6c7386437ea9ab34be033ecfedf60ac5f5439741
Lizandro Zerpa <lizandroal.zerpa@gmail.com>
2018-05-19 17:37:12 -0400

    BACKEND config

---
+++ apps/user/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
+
+
+class People(models.Model):
+    """
+    Model for User Type
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    ci = models.CharField(max_length=10, unique=True,
+                          validators=[RegexValidator
+                                      (regex="^[0-9]{10}$",
+                                       message="Invalid format for CI")])
+    profileImage = models.FileField(upload_to='uploads/')
+
+    def __str__(self):
+        return self.user.username
+
+
+class Teacher(People):
+    """
+    Model for Teacher Type
+    """
+    root = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Student(People):
+    """
+    Model for Student Type
+    """
+    progress = models.CharField(max_length=10)
+    teacherMentor = models.ForeignKey('user.Teacher', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
