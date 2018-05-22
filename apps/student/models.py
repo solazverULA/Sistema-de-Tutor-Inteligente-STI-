aa9452371c110996766eccbf1cbd1b87338dfb80
LizanLycan <lizandroal.zerpa@gmail.com>
2018-05-21 20:50:44 -0400

    Fixing Models for Templates

--- apps/student/models.py
+++ apps/student/models.py
@@ -1,13 +1,3 @@
 from django.db import models
-from apps.user.models import People
 
-
-class Student(People):
-    """
-    Model for Student Type
-    """
-    progress = models.CharField(max_length=10)
-    teacherMentor = models.ForeignKey('user.Teacher', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.user.username
+# Create your models here.
--- apps/teacher/models.py
+++ apps/teacher/models.py
@@ -1,12 +1,3 @@
 from django.db import models
-from apps.user.models import People
 
-
-class Teacher(People):
-    """
-    Model for Teacher Type
-    """
-    root = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.user.username
+# Create your models here.
