e96255fa43a075cea390034f80b912d76e0a7efe
sarait <saraithernandez0205@gmail.com>
2018-05-28 19:07:40 -0400

    templates finales

--- apps/student/forms.py
+++ apps/student/forms.py
@@ -6,8 +6,8 @@ from apps.teacher.models import Teacher
 
 
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(max_length=30, required=True, label='Nombre de Usuario', help_text="study")
-    password = forms.CharField(max_length=30, required=True, label="Contrasenia", help_text="key")
+    username = forms.CharField(max_length=30, required=True, label='Nombre de usuario', help_text="study")
+    password = forms.CharField(max_length=30, required=True, label="Contraseña", help_text="key")
 
     class Meta:
         model = User
@@ -20,14 +20,14 @@ class TeacherModelChoiceField(ModelChoiceField):
 
 
 class SignUpForm(UserCreationForm):
-    username = forms.CharField(max_length=30, required=True, label='Nombre de Usuario', help_text="study")
-    ci = forms.CharField(max_length=10, label="Cédula de Identidad", help_text="id")
-    first_name = forms.CharField(max_length=30, required=False, label='Nombre', help_text="study")
-    last_name = forms.CharField(max_length=30, required=False, label="Apellido", help_text="study")
-    email = forms.EmailField(max_length=254, label="Correo Electrónico", help_text="mail")
-    teacherMentor = TeacherModelChoiceField(queryset=Teacher.objects.all(), help_text="smile")
-    password1 = forms.CharField(max_length=30, required=True, label="Contrasenia", help_text="key")
-    password2 = forms.CharField(max_length=30, required=True, label="Repita Contrasenia", help_text="key")
+    username = forms.CharField(max_length=30, required=True, label='Nombre de usuario', help_text="user")
+    ci = forms.CharField(max_length=10, label="Cédula de identidad", help_text="id")
+    first_name = forms.CharField(max_length=30, required=False, label='Nombre', help_text="smile")
+    last_name = forms.CharField(max_length=30, required=False, label="Apellido", help_text="wallet")
+    email = forms.EmailField(max_length=254, label="Correo electrónico", help_text="mail")
+    teacherMentor = TeacherModelChoiceField(queryset=Teacher.objects.all(), label="Profesor", help_text="study")
+    password1 = forms.CharField(max_length=30, required=True, label="Contraseña", help_text="key")
+    password2 = forms.CharField(max_length=30, required=True, label="Repita la contraseña", help_text="key")
 
     class Meta:
         model = User
